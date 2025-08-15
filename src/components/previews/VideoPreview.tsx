65a01a901d991e564fbf4029564fabdba9b924e7
StudyLong <46282071+LongStudy@users.noreply.github.com>
2025-08-15 22:58:10 +0800

    Update VideoPreview.tsx

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -61,6 +61,7 @@ const VideoPlayer: FC<{
     type: 'video',
     title: videoName,
     poster: thumbnail,
+    sources: [] as { src: string; type?: string }[],
     tracks: [{ kind: 'captions', label: videoName, src: '', default: true, srclang: 'zh', }],
   }
   const plyrOptions: Plyr.Options = {
